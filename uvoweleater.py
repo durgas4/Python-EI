b24d18d638472574ae50ef341c4937684bc26caa
sadasivuni durga <39164201+durgas4@users.noreply.github.com>
2021-03-23 23:35:58 +0530

    Vowel eater

---
+++ uvoweleater.py
@@ -0,0 +1,17 @@
+# Prompt the user to enter a word\
+
+word=input("enter a word")
+
+# and assign it to the user_word variable.
+for i in word:
+    # Complete the body of the for loop.
+    if i=="a":
+        continue
+    elif i=="e":#conditional execution
+        continue
+    elif i=="o":
+        continue
+    elif i=="u":
+        continue
+    else:
+        print(i)
