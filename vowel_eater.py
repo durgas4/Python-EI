26182123b91a1985b17f2a4206fe71cfdf588ad7
sadasivuni durga <39164201+durgas4@users.noreply.github.com>
2021-03-23 23:21:37 +0530

    Add files via upload
    Vowel eater

---
+++ vowel_eater.py
@@ -0,0 +1,18 @@
+# Prompt the user to enter a word\
+user_word=""
+word=str(input("enter a word"))
+# and assign it to the user_word variable.
+for i in word:
+    # Complete the body of the for loop.
+    if i=="a":
+
+        continue
+    elif i=="e":#conditional execution
+        continue
+    elif i=="o":
+        continue
+    elif i=="u":
+        continue
+    else:
+        user_word+=i
+        print(user_word)
